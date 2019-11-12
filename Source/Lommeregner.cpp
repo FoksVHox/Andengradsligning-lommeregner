306fced01747876ae88c5a854c89d7117f83a221
Glymse <glymsedk@gmail.com>
2019-11-12 18:04:13 +0100

    Gjort så programmet ikke lukker med det samme efter brug

--- Source/Lommeregner.cpp
+++ Source/Lommeregner.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <math.h> 
+#include <sstream> 
 
 using namespace std;
 double Dval;
 double Aval;
 double Bval;
 double Cval;
-int DvDDDal;
+string CloseOrNotCin;
 
-	// Funktionen udregner de 2 x varianter
+
+// Funktionen for hvis der er 2x'er
 void ToX() {
 	// Minus-x udregning
-	double Minusx1 = -Bval-sqrt(Dval);
-	double Minusx2 = 2*Aval;
+	double Minusx1 = -Bval - sqrt(Dval);
+	double Minusx2 = 2 * Aval;
 	double MinusR = Minusx1 / Minusx2;
 
 	// Plus-x udregning
@@ -21,84 +23,115 @@ void ToX() {
 	double PlusxR = Plusx1 / Plusx2;
 
 	// Udregning af d
-	cout << "Udregning af d \n" << endl;
-	cout << Bval<<"^2 "<<"- "<< "4*"<<Aval << Cval << " = "<< Dval<<"\n" <<endl;
+	cout << "Udregning af d: \n" << endl;
+	cout << Bval << "^2 " << "- " << "4*" << Aval << Cval << " = " << Dval << "\n" << endl;
 
 	// Udregning af x2
-	cout << "Udregning af x2 \n" << endl;
+	cout << "Udregning af x2: \n" << endl;
 
 	cout << "-" << Bval << " + kvrod(" << Dval << ") = " << Minusx1 << "\n" << Aval << "^2 = " << Minusx2 << endl;
 	cout << Minusx1 << "/" << Minusx2 << endl;
 	cout << MinusR << "\n" << endl;
 
 	// Udregning af x1
-	cout << "Udregning af x1 \n" <<endl;
+	cout << "Udregning af x1: \n" << endl;
 
-	cout << "-" << Bval << " - kvrod(" << Dval<< ") = "<< Plusx1 <<"\n"<< Aval << "^2 = " << Plusx2 << endl;
-	cout <<Plusx1 <<"/"<<Plusx2 << endl;
-	cout << PlusxR <<"\n"<<endl;
+	cout << "-" << Bval << " - kvrod(" << Dval << ") = " << Plusx1 << "\n" << Aval << "^2 = " << Plusx2 << endl;
+	cout << Plusx1 << "/" << Plusx2 << endl;
+	cout << PlusxR << "\n" << endl;
 
 	// Endeligt resultat
-	cout <<"x=" << MinusR << " eller " << "x="<< PlusxR <<"\n"<<endl;
+	cout << "x=" << MinusR << " eller " << "x=" << PlusxR << "\n" << endl;
 
 	cout << "//////////////////////////////////" << endl;
 	cout << "Lommeregner af Jonas Glymov" << endl;
 }
 
-	// Funktion for hvis diskriminanten er 0
+// Funktion for hvis diskriminanten er 0
 void EnX() {
 
 	double EnX = -Bval / 2 * Aval;
-	cout << "Udregning af x1 \n" << endl;
-	cout <<"-" << Bval << " / " << "2*"<<Aval << endl;
-	cout <<"x= "<< EnX << endl;
+	cout << "Udregning af x1: \n" << endl;
+	cout << "-" << Bval << " / " << "2*" << Aval << endl;
+	cout << "x= " << EnX << endl;
 
 }
+
+
 
 int main()
 {
-	// Indtast a værdi
+
+
+
+	// Get ax value
 	cout << "//////////////////////////////\n" << endl;
-	cout << " indtast a, b og c vaerdier\n" << endl;
+	cout << "  indtast a, b og c vaerdier\n" << endl;
+	cout << "       ax^2 + bx + c = 0\n" << endl;
 	cout << "//////////////////////////////\n" << endl;
-	cout << "indtast 'a' vaerdi" << endl;
+	cout << "								\n" << endl;
+
+	cout << "indtast venligts 'a'x vaerdien af din ligning" << endl;
 	cin >> Aval;
-
-	// Indtast b værdi
-	cout << "indtast 'b' vaerdi" << endl;
+	cout << "								\n" << endl;
+	// Get bx value
+	cout << "indtast venligts 'a'x vaerdien af din ligning" << endl;
 	cin >> Bval;
-
-	// Indtast c værdi
-	cout << "indtast 'c' vaerdi" << endl;
+	cout << "								\n" << endl;
+	// Get c value
+	cout << "indtast venligts 'a'x vaerdien af din ligning" << endl;
 	cin >> Cval;
+	cout << "								\n" << endl;
+	// Regn 'd' Værdien
+	Dval = Bval * Bval - 4 * Aval * Cval;
 
-	// Udregner d værdi
-	Dval = Bval*Bval - 4 * Aval * Cval;
-	
+	cout << "								\n" << endl;
 	// Interface
 	cout << "//////////////////////////////\n" << endl;
-	cout << "UDREGNING\n" << endl;
+	cout << "          UDREGNING\n" << endl;
 	cout << "//////////////////////////////\n" << endl;
-
+	cout << "								\n" << endl;
 	// Diskriminent tjekker
 	if (Dval < 0) {
-		cout << "Udregning af d \n" << endl;
+		cout << "Udregning af d: \n" << endl;
 		cout << Bval << "^2 " << "- " << "4*" << Aval << Cval << " = " << Dval << "\n" << endl;
 		cout << "Ligningen har ingen losninger, da deskriminanten er negativ!\n" << endl;
+		cout << "								\n" << endl;
+		cout << "								\n" << endl;
 		cout << "//////////////////////////////////" << endl;
-		cout << "Lommeregner af Jonas Glymov" << endl;
-}
+		cout << "   Lommeregner af Jonas Glymov" << endl;
+		cout << "//////////////////////////////////" << endl;
+		cout << "								\n" << endl;
+		cout << "Vil du lukke programmet? 'y' eller 'n':" << endl;
+	}
 	// Tjekker hvorvidt d er over 0
-		else if (Dval > 0) {
+	else if (Dval > 0) {
 
-		
+
 		ToX();
 
-}
+	}
 	//// Tjekker hvorvidt d er 0
-		else if (Dval == 0) {
+	else if (Dval == 0) {
 		EnX();
 
 	}
-	cin >> DvDDDal;
+
+	/* Ja eller nej til at lukke program :))*/
+	cin >> CloseOrNotCin;
+	if (CloseOrNotCin == "n") {
+		cout << "			" << endl;
+		for (int i = 0; i < 15; i++) {
+			cout << "			\n" << endl;
+		}
+		main();
+	}
+	else if (CloseOrNotCin == "y") {
+		exit(0);
+	}
+	else if (CloseOrNotCin != "n" && "y") {
+		exit(0);
+	}
+
 }
+
